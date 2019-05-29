bd245f251b1cf456229df5738c4291afaa6fa9d0
Xuan Do <doxuancntp@gmail.com>
2019-05-29 17:28:08 -0400

    Mixins.

--- mixins/js/app.js
+++ mixins/js/app.js
@@ -1,9 +1,42 @@
+const Child = {
+    template: '#childarea'
+};
+
+const toggle = {
+    data() {
+        return {
+            isShowing: false
+        }
+    },
+    methods: {
+        toggleShow() {
+            this.isShowing = !this.isShowing;
+        }
+    }
+}
+
+const Modal = {
+    template: '#modal',
+    mixins: [toggle],
+    components: {
+        appChild: Child
+    }
+};
+
+const Tooltip = {
+    template: '#tooltip',
+    mixins: [toggle],
+    components: {
+        appChild: Child
+    }
+};
+
+
+
 new Vue({
     el: "#app",
-    data: {
-
-    },
-     methods: {
-         
-     }
+    components: {
+        appModal: Modal,
+        appTooltip: Tooltip
+    }
 });
