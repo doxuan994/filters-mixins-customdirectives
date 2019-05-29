859a8d3eb956b8380b573ab37c8f5fe52015ac97
Xuan Do <doxuancntp@gmail.com>
2019-05-29 15:54:35 -0400

    Chained Filters.

--- filters/js/app.js
+++ filters/js/app.js
@@ -2,18 +2,25 @@ new Vue({
     el: "#app",
     data() {
         return {
-            customer1total: 35.43
+            customer1total: 35.43,
+            num: 2
         }
     },
     filters: {
         tip15(value) {
-            return (value*0.15).toFixed(2);
+            return (value*0.15).toFixed(2)
         },
         tip20(value) {
-            return (value*0.2).toFixed(2);
+            return (value*0.2).toFixed(2)
         },
         tip25(value) {
-            return (value*0.25).toFixed(2);
+            return (value*0.25).toFixed(2)
+        },
+        plus5(value) {
+            return value + 5
+        },
+        times2(value) {
+            return value * 2
         }
     }
 });
