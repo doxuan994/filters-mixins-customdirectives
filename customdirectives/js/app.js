8193dfcb6ca1b2ab9c2a073ba37e5beb498d2dbc
Xuan Do <doxuancntp@gmail.com>
2019-05-29 20:42:55 -0400

    Simple directive with two values.

--- customdirectives/js/app.js
+++ customdirectives/js/app.js
@@ -2,9 +2,12 @@ Vue.directive('tack', {
     bind: function(el, binding, vnode) {
         el.style.position = 'fixed';
 
-        const s = (binding.arg == 'left' ? 'left' : 'top');
+        // const s = (binding.arg == 'left' ? 'left' : 'top');
 
-        el.style[s] = binding.value + 'px';
+        // el.style[s] = binding.value + 'px';
+
+        el.style.top = binding.value.top + 'px';
+        el.style.left = binding.value.left + 'px';
     }
 });
 
